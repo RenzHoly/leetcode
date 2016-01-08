3b317c65dde6577ff700d2d3b75fd826ed0e9d20
RenzHoly <renzholy@gmail.com>
2016-01-08 21:24:46 +0800

    problem 1

--- 1-two-sum.js
+++ 1-two-sum.js
@@ -1,14 +1,15 @@
 var twoSum = function (nums, target) {
-    var temp = {};
-    var index;
-    for (index = 0; index < nums.length; index++) {
-        temp[target - nums[index]] = index;
+    var temp = [];
+    var i;
+    for (i = 0; i < nums.length; i++) {
+        temp[target - nums[i]] = i;
     }
-    for (index = 0; index < nums.length; index++) {
-        if (temp[nums[index]]) {
-            return [index + 1, temp[nums[index]] + 1];
+    for (i = 0; i < nums.length; i++) {
+        if (temp[nums[i]] && temp[nums[i]] != i) {
+            return [i + 1, temp[nums[i]] + 1];
         }
     }
 };
 
-console.log(twoSum([3, 2, 4], 6));
+console.log(twoSum([1, 3, 2, 4], 6));
+console.log(twoSum([3, 1, -3], 0));
